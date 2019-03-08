e76ec416e4aff5755a8be67499a93e7c8b86deca
rm5565 <richard@randkmoore.com>
2019-03-08 14:24:24 -0800

    added callback support for Solar functions

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,18 @@
 
-// getSiteInfo("961882");
+getSiteInfo("961882", function(siteInfo) {
+
+    console.log("From getSiteInfo callback"); 
+    console.log(siteInfo);
+}); 
 
 
 
 // Unix Date Jan 1, 2019 : 1546329600
 // Unix date Feb 8, 2019 : 1552032000
 // 
-getProductionHistory("961882", 1546329600, 1552032000);
+getProductionHistory("961882", 1546329600, 1552032000, function(solarData) {
+
+    console.log("From getProductionHistory callback ");
+    console.log(solarData);
+});
+
