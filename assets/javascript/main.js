4dee27b021f2f78fcd70e64c885a77c737eb48e0
rm5565 <richard@randkmoore.com>
2019-03-09 21:41:09 -0800

    Added graphing module to produce bar chart for last 14 days using Google Charts

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,18 +1,40 @@
-
+/* 
 getSiteInfo("961882", function(siteInfo) {
 
     console.log("From getSiteInfo callback"); 
     console.log(siteInfo);
 }); 
-
+ */
 
 
 // Unix Date Jan 1, 2019 : 1546329600
 // Unix date Feb 8, 2019 : 1552032000
 // 
-getProductionHistory("961882", 1546329600, 1552032000, function(solarData) {
 
+var gsolarData;
+
+
+var EndDate = moment().format();
+var StartDate = moment(EndDate).subtract(14, "days");
+console.log("StartDate = " + StartDate + " EndDate = " + EndDate);
+
+
+
+
+
+
+
+getProductionHistory("961882", moment(StartDate).unix(), moment(EndDate).unix(), function(solarData) {
+
+
+    // getProductionHistory("961882", 1546329600, 1552032000, function(solarData) {
     console.log("From getProductionHistory callback ");
     console.log(solarData);
+    gsolarData = solarData;
+
+    google.charts.load('current', {'packages':['corechart']});     
+    
+    google.charts.setOnLoadCallback(drawChart);
+
 });
 
