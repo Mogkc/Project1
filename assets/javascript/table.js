2fab661c707c32f02de8f07375eafbfe99490954
Mogkc <carson.mogk@cox.net>
2019-03-11 10:35:52 -0700

    tested and updated table.js

--- assets/javascript/table.js
+++ assets/javascript/table.js
@@ -8,28 +8,37 @@
 var displayRow = function(date, weatherPic, weatherText, energy) {
     var newRow = $("<tr>");
     //Leftmost element is date
-    var day = date.getFullYear() + "/" + (date.getMonth()+1) + "/" + date.getDate();
-    var dispDay = $("<td>").text(day);
+    var day = date.format("dddd, MMMM Do");
+    var dispDay = $("<td>");
+    dispDay.text(day);
     dispDay.attr("scope", "row");
 
     newRow.append(dispDay);
     //Middle element holds the weather
+    var dispWeath = $("<td>") ;
+    dispWeath.attr("scope", "row");
     var button = $("<button>");
     //Format the button so it can show popover text
+    button.attr("type", "button");
     button.attr("class", "btn btn-link p-0");
     button.attr("data-container", "body");
     button.attr("data-toggle", "popover");
     button.attr("data-placement", "right");
     button.attr("data-content", weatherText);
+    button.attr("data-trigger", "focus");
     //Make the button hold the image
     var img = $("<img>");
     img.attr("src", weatherPic);
     button.append(img);
+    dispWeath.append(button);
 
-    newRow.append($("<td>").append(button) );
+    newRow.append(dispWeath);
     //Rightmost element is energy prediction
-    newRow.append("<td>").text(energy + " kW hours");
+    var energyDisp = $("<td>");
+    energyDisp.attr("class", "text-right");
+    energyDisp.text(energy + " kW hours");
 
+    newRow.append(energyDisp);
     //Append the row to the existing table
     $("#tableData").append(newRow);
 }
