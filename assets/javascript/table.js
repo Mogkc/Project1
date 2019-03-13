8cc58802feb93b5c28377724196ce6498e5a2369
cfcrotty <carafeliselana@gmail.com>
2019-03-13 16:43:42 -0700

    update popover UI

--- assets/javascript/table.js
+++ assets/javascript/table.js
@@ -52,7 +52,6 @@ var displayRow = function (date, weatherPic, weatherText, energy) {
     img.attr("class", "icon");
     img.attr("src", "assets/images/weather/" + weatherPic + ".png");
     button.append(img);
-    //dispWeath.append(button.popover());
     dispWeath.append(button.popover({
         html: true
     }));
